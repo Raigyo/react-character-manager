500be9167185908d33aafec60e05fdaf693196f2
Raigyo <vincent.chilot@gmail.com>
2019-03-25 16:21:09 +0100

    Updates

---
+++ precache-manifest.30811335c7be6046d84427a6e7514227.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e05636f0475ba647e1a10951e662e7e7",
+    "url": "/react-character-manager/static/media/deadpool-logo.e05636f0.png"
+  },
+  {
+    "revision": "6d242375651bc27cc5a1",
+    "url": "/react-character-manager/static/js/runtime~main.0c53a67b.js"
+  },
+  {
+    "revision": "ab7ff53db3212db53eda",
+    "url": "/react-character-manager/static/js/main.7cb02e09.chunk.js"
+  },
+  {
+    "revision": "6d9030caca0cf89b5f03",
+    "url": "/react-character-manager/static/js/2.14ab0029.chunk.js"
+  },
+  {
+    "revision": "ab7ff53db3212db53eda",
+    "url": "/react-character-manager/static/css/main.75b04c1b.chunk.css"
+  },
+  {
+    "revision": "6d9030caca0cf89b5f03",
+    "url": "/react-character-manager/static/css/2.6f14bdd9.chunk.css"
+  },
+  {
+    "revision": "9aa58e76125abba5908e3b05953d0e5d",
+    "url": "/react-character-manager/index.html"
+  }
+];
